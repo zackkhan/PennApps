1a8984459d712df05193bf5354a03aa9db2d2274
Zachariyya Khan <zack.khan101@gmail.com>
2017-01-21 12:05:58 -0500

    adding http requests and more voice commands

--- PennAppsAndroid/app/src/main/java/com/example/kodactive/pennapps/MainActivity.java
+++ PennAppsAndroid/app/src/main/java/com/example/kodactive/pennapps/MainActivity.java
@@ -1,23 +1,45 @@
 package com.example.kodactive.pennapps;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Locale;
+
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Network;
+import android.net.http.RequestQueue;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.reimaginebanking.api.java.NessieClient;
 import com.reimaginebanking.api.java.NessieException;
 import com.reimaginebanking.api.java.NessieResultsListener;
 import com.reimaginebanking.api.java.models.Customer;
 import com.reimaginebanking.api.java.models.Purchase;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import retrofit.client.Response;
 
 import static java.lang.Integer.parseInt;
 
@@ -29,6 +51,93 @@ public class MainActivity extends Activity {
     private ImageButton btnMicrophone;
     public static NessieClient nessieClient = NessieClient.getInstance();
     TextToSpeech ttobj;
+    URL url;
+    HttpURLConnection urlConnection = null;
+
+    public void detectCrosswalk() {
+        // Instantiate the RequestQueue.
+        com.android.volley.RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://penn-apps.herokuapp.com/test";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println("RESPONSEHERE");
+                        System.out.println(response);
+
+                        // Display the first 500 characters of the response string.
+                        // mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+
+    public void whatIsThat() {
+        // Instantiate the RequestQueue.
+        com.android.volley.RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://penn-apps.herokuapp.com/whatisthat";
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        System.out.println("RESPONSEHERE");
+                        System.out.println(response);
+
+                        // Display the first 500 characters of the response string.
+                        // mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("That didn't work!");
+            }
+        });
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
+
+    public void nessieIntegration(String text, String x)
+    {
+        if (text.contains("dollars") || text.contains("dollar") || text.contains("$")) {
+            for (int i = 0; i < text.length(); i++) {
+                if (Character.isDigit(text.charAt(i))) {
+                    x += text.charAt(i);
+                }
+            }
+            final int userAmount = Integer.parseInt(x);
+            nessieClient.getPurchases("5877e7481756fc834d8eace6", new NessieResultsListener() {
+                @Override
+                public void onSuccess(Object o, NessieException e) {
+                    ArrayList<Purchase> purchases = (ArrayList<Purchase>) o;
+                    String amt = purchases.get(purchases.size() - 1).toString();
+                    String amtString = amt.substring(amt.indexOf("amount"));
+                    String finalAmtString = amtString.substring(amtString.indexOf('=') + 1, amtString.indexOf(','));
+                    System.out.println("LOOK HERE HAHAHAHAAHAHAH");
+                    double amount = Double.parseDouble(finalAmtString);
+                    System.out.println(amount);
+                    if ((double) userAmount == amount) {
+                        ttobj.speak("That was a valid transaction. Your most recent transaction was" + amount + "dollars", TextToSpeech.QUEUE_FLUSH, null);
+                    } else {
+                        ttobj.speak("That transaction was invalid. You stated the amount was" + userAmount + "but " + amount + "was removed from your account", TextToSpeech.QUEUE_FLUSH, null);
+                    }
+
+
+                }
+            });
+        }
+    }
+
 
 
     @Override
@@ -39,14 +148,12 @@ public class MainActivity extends Activity {
         txtOutput = (TextView) findViewById(R.id.txt_output);
         btnMicrophone = (ImageButton) findViewById(R.id.btn_mic);
 
-        ttobj =new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+        ttobj = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
                 ttobj.setLanguage(Locale.UK);
             }
         });
-
-
 
         btnMicrophone.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,10 +161,11 @@ public class MainActivity extends Activity {
                 startSpeechToText();
             }
         });
-    }
+
+
     /**
      * Start speech to text intent. This opens up Google Speech Recognition API dialog box to listen the speech input.
-     * */
+     */
     private void startSpeechToText() {
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
@@ -73,9 +181,10 @@ public class MainActivity extends Activity {
                     Toast.LENGTH_SHORT).show();
         }
     }
+
     /**
      * Callback for speech recognition activity
-     * */
+     */
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -87,42 +196,13 @@ public class MainActivity extends Activity {
                     String text = result.get(0);
                     txtOutput.setText(text);
                     String x = "";
-                    if (text.contains("dollars") || text.contains("dollar") || text.contains("$"))
-                    {
-                        for (int i=0; i<text.length(); i++)
-                        {
-                            if (Character.isDigit(text.charAt(i)))
-                            {
-                                x += text.charAt(i);
-                            }
-                        }
-                        final int userAmount = Integer.parseInt(x);
-nessieClient.getPurchases("5877e7481756fc834d8eace6", new NessieResultsListener() {
-    @Override
-    public void onSuccess(Object o, NessieException e) {
-        ArrayList<Purchase> purchases = (ArrayList<Purchase>) o;
-        String amt = purchases.get(purchases.size()-1).toString();
-       String amtString = amt.substring(amt.indexOf("amount"));
-        String finalAmtString = amtString.substring(amtString.indexOf('=')+1, amtString.indexOf(','));
-        System.out.println("LOOK HERE HAHAHAHAAHAHAH");
-        double amount = Double.parseDouble(finalAmtString);
-        System.out.println(amount);
-if ((double) userAmount == amount)
-{ ttobj.speak("That was a valid transaction. Your most recent transaction was" + amount + "dollars", TextToSpeech.QUEUE_FLUSH, null );}
-        else  {
-    ttobj.speak("That transaction was invalid. You stated the amount was" + userAmount + "but " + amount + "was removed from your account",  TextToSpeech.QUEUE_FLUSH, null);
-        }
-
-
-
-    }
-});
-                    }
+                    nessieIntegration(text, x);
                 }
                 break;
             }
         }
-    }}
+    }
+}
 /*
 import android.content.BroadcastReceiver;
 import android.content.Context;
