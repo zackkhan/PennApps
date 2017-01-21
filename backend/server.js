0ea3e4ab26006a6e6fa2a6316f43ed319623d0a2
Michael <m.anderjaska@gmail.com>
2017-01-21 03:15:11 -0500

    ok backend/client

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,58 @@
 'use strict';
 
-//express
-var app = require('express')();
-var server = require('http').Server(app);
-//socket.io
+var url = require("url");
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require('path');
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-//bodyParser
-var bodyParser = require('body-parser');
+//require("babel-core");
+
+var port = process.env.PORT || 5000;
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
 
-
-const port = process.env.PORT || 3000;
-
-io.on('connection',  (socket) => {
-  //io.emit('this', { will: 'be received by everyone'});
-
-  socket.on('send-image', (image) => {
-    console.log('image received');
-    console.log(image.hello);
-    //do stuff with image
-  });
-
-  socket.on('disconnect', function () {
-    io.emit('user disconnected');
-  });
+server.listen(port, () => {
+  console.log('App is online on ' + port);
 });
 
+app.get('getPort', (req, res) =>{
+    res.send(JSON.stringify({'port': port});
+    
+})
 
 app.get('/test', function(request, response) {
   console.log('test request received');
   response.send('test complete');
 });
 
+app.get('/startStream', (req, res) => {
+    io.emit('start-streaming');
+    res.sendStatus(200);
+})
 
-app.listen(port, () => {
-  console.log('App is online on ' + port);
+app.get('/endStream', (req, res) => {
+     io.emit('end-streaming');
+     res.sendStatus(200);
+})
+
+app.post('/sensorData', (req,res) =>{
+    //do something with our sensor data
+})
+
+//io with images
+
+io.on('connection',  (socket) => {
+  //io.emit('this', { will: 'be received by everyone'});
+
+
+  socket.on('send-image', (image) => {
+    //do stuff with image (i.e. apis)
+    console.log('image received');
+  });
+
+  socket.on('disconnect', function () {
+      console.log("disconnected")
+  });
 });
--- client/client.js
+++ client/client.js
@@ -1,20 +1,87 @@
+var express = require('express');
+var app = express();
+var request = require('request');
+var bodyParser = require("body-parser");
 var io = require('socket.io-client');
 //var socket = io('penn-apps.herokuapp.com:51356');
-var socket = io('http://localhost:5000');
+var socket = null;
 
+//file stuff
 var base64 = require('node-base64-image');
+var fs = require('fs')
+
+//syscall
+var spawn = require('child_process').spawn;
+var proc;
+
+app.use(bodyParser.json({
+	limit : config.bodyLimit
+}));
 
 var path = __dirname + '/image.jpg';
-
 options = {string: true, local: true};
 
+
+request('http://penn-apps.herokuapp.com/getPort',  (error, response, body) => {
+  if (!error && response.statusCode == 200) {
+    socket = io('http://penn-apps.herokuapp.com:' + body.port);
+    console.log('client connected to port ' + body.port) // Show the HTML for the Google homepage.
+
+  }
+})
+
+//while(socket == null){};
+/*
 socket.on('connect', function(){
     console.log('connected!')
 });
 
-base64.encode(path, options, function (err, image) {
-    if (err) {console.log(err); }
-//console.log(image);
-  socket.emit('send-image', image);
-
+socket.on('start-streaming', () => {
+   console.log('streaming initialized')
+   startStreaming();
 });
+
+socket.on('end-streaming', () => {
+   console.log('streaming terminated')
+   stopStreaming();
+});
+
+*/
+//sends image to backend
+function sendImage(){
+    base64.encode(path, options, function (err, image) {
+        if (err) {console.log(err); }
+
+       socket.emit('send-image', image);
+
+    });
+}
+
+
+function stopStreaming() {
+
+    app.set('watchingFile', false);
+    if (proc) proc.kill();
+    fs.unwatchFile(path);
+
+}
+
+function startStreaming() {
+
+  if (app.get('watchingFile')) {
+    sendImage();
+    return;
+  }
+  /*
+  var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
+  proc = spawn('raspistill', args);
+  */
+  console.log('Watching for changes...');
+
+  app.set('watchingFile', true);
+
+  fs.watchFile(path, function(current, previous) {
+    sendImage();
+  })
+
+}
